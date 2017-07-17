b6c5e38cb96d159bf5401eab230d334825b6d037
Sean Carver <seancarverphd@gmail.com>
2017-07-17 07:54:55 -0400

    Adding Reps.R file to repo

---
+++ reps.R
@@ -0,0 +1,14 @@
+repeated.estimates <- function(reps=6, seed=FALSE, quantile.regression=TRUE) {
+  if (seed != FALSE) {
+    set.seed(seed)
+  }
+  estimates <- c()
+  for (k in 1:reps) {
+    bs.t5 <- sim.t(200000, df=5)
+    ll.t5.t5 <- likes.t(bs.t5, df=5)
+    ll.t5.normal <- likes.t(bs.t5, df=Inf)
+    estimates[k] <- samples.needed(ll.t5.t5, ll.t5.normal, 
+                                   max.samples=250, bootstrap.rows=1000, quantile.regression=quantile.regression)
+  }
+  return(estimates)
+}
