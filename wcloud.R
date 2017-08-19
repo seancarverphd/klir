b956ffbf60c719d11f6274fb7558cb699589fd1b
Sean Carver <seancarverphd@gmail.com>
2017-08-18 21:29:27 -0400

    transitions.from() coded

--- wcloud.R
+++ wcloud.R
@@ -11,7 +11,7 @@ freq <- round(exp(likes.baseball(innings, BAL))*2500)
 wordcloud(innings,freq,random.order=TRUE,colors='black')
 #wordcloud(innings,freq,random.order=TRUE,colors=brewer.pal(8,'RdBu'))
 
-list.from.string <- function(from.string) {
+transitions.from <- function(from.string) {
   batter <- TRUE
   runner1 <- grepl('1',from.string)
   runner2 <- grepl('2',from.string)
@@ -23,35 +23,37 @@ list.from.string <- function(from.string) {
 num.equal <- function(x, y) {
   return(is.numeric(x) & x==y)
 }
+
 list.transitions <- function(batter, runner1, runner2, runner3, outs) {
   trans.list <- c()
-  do.return <- FALSE
   if (isTRUE(batter)) {
-    do.return <- TRUE
     for (new.batter in 0:4) {
-      trans.list <- c(trans.list, list.transitions(new.batter, runner1, runner2, runner3, outs))
+      more.transitions <- list.transitions(new.batter, runner1, runner2, runner3, outs)
+      trans.list <- c(trans.list, more.transitions)
+      #print(more.transitions)
     }
+    return(unique(trans.list))
   }
   if (isTRUE(runner1)) {
-    do.return <- TRUE
     for (new.runner1 in 0:4) {
-      print(c("batter",batter,"new.runner1",new.runner1))
-      trans.list <- c(trans.list, list.transitions(batter, new.runner1, runner2, runner3, outs))
+      #print(new.runner1)
+      more.transitions <- list.transitions(batter, new.runner1, runner2, runner3, outs)
+      trans.list <- c(trans.list, more.transitions)
+      #print(more.transitions)
     }
+    return(unique(trans.list))
   }
   if (isTRUE(runner2)) {
     do.return <- TRUE
     for (new.runner2 in 0:4) {
       trans.list <- c(trans.list, list.transitions(batter, runner1, new.runner2, runner3, outs))
     }
+    return(unique(trans.list))
   }
   if (isTRUE(runner3)) {
-    do.return <- TRUE
     for (new.runner3 in 0:4) {
       trans.list <- c(trans.list, list.transitions(batter, runner1, runner2, new.runner3, outs))
     }
-  }
-  if (do.return) {
     return(unique(trans.list))
   }
   batter.out <- num.equal(batter, 0)
@@ -91,12 +93,12 @@ list.transitions <- function(batter, runner1, runner2, runner3, outs) {
   } else {
     zero.string <- ''
   }
-  legit <- ((batter < runner1) | num.equal(runner1, 0) | identical(runner1, FALSE)) &
-           ((batter < runner2) | num.equal(runner2, 0) | identical(runner2, FALSE)) &
-           ((batter < runner3) | num.equal(runner3, 0) | identical(runner3, FALSE)) &
-           ((runner1 < runner2) | num.equal(runner2, 0) | identical(runner1, FALSE) | identical(runner2, FALSE)) &
-           ((runner1 < runner3) | num.equal(runner3, 0) | identical(runner1, FALSE) | identical(runner3, FALSE)) &
-           ((runner2 < runner3) | num.equal(runner3, 0) | identical(runner2, FALSE) | identical(runner3, FALSE))
+  legit <- ((batter < runner1) | (num.equal(batter, 4) & num.equal(runner1, 4)) | num.equal(runner1, 0) | identical(runner1, FALSE)) &
+           ((batter < runner2) | (num.equal(batter, 4) & num.equal(runner2, 4)) | num.equal(runner2, 0) | identical(runner2, FALSE)) &
+           ((batter < runner3) | (num.equal(batter, 4) & num.equal(runner3, 4)) | num.equal(runner3, 0) | identical(runner3, FALSE)) &
+           ((runner1 < runner2) | (num.equal(runner1, 4) & num.equal(runner2, 4)) | num.equal(runner2, 0) | identical(runner1, FALSE) | identical(runner2, FALSE)) &
+           ((runner1 < runner3) | (num.equal(runner1, 4) & num.equal(runner3, 4)) | num.equal(runner3, 0) | identical(runner1, FALSE) | identical(runner3, FALSE)) &
+           ((runner2 < runner3) | (num.equal(runner2, 4) & num.equal(runner3, 4)) | num.equal(runner3, 0) | identical(runner2, FALSE) | identical(runner3, FALSE))
   if (legit) {
     return.string <- paste(zero.string,one.string,two.string,three.string,out.string,sep='')
     return(unique(return.string))
